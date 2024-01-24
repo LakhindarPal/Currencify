709190d09afaf99c8dd61bf2e231248b1249a864
72681474+LakhindarPal@users.noreply.github.com <72681474+LakhindarPal@users.noreply.github.com>
2024-01-24 17:37:13 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# currencify
-Convert currencies easily with Currencify. Simple and efficient currency converter.
+# Currencify - Currency Converter
+
+Currencify is a simple and efficient currency converter web app. Easily convert currencies with a user-friendly interface.
+
+## Features
+
+- **Progressive Web App (PWA):** Experience the convenience of a Progressive Web App, allowing you to use Currencify seamlessly, even offline. Install it on your device for quick access and an app-like experience directly from your home screen.
+
+- **Dark Mode:** Enjoy a comfortable viewing experience in low-light environments with the Dark Mode feature.
+  
+- **Responsive Design:** Currencify adapts to various screen sizes, providing a seamless experience across devices, including desktops, tablets, and smartphones.
+
+- **Over 40 Supported Currencies:** Choose from a diverse range of over 40 currencies for your conversions, making it easy to work with different international currencies.
+
+- **Cryptocurrency Exchange Rates Included:** Stay up-to-date with the latest cryptocurrency exchange rates, offering a comprehensive solution for both traditional and digital currencies.
+
+## Live Demo 
+
+[https://currencify.glitch.me](https://currencify.glitch.me)
+
+## Technologies
+
+- **HTML:** The structure of the web pages.
+- **CSS:** Styling for a visually appealing user interface.
+- **JavaScript (ES6):** Functionality and interactivity of the currency converter.
+- **Service Worker:** Enables the use of progressive web app features, such as offline functionality.
+- **Manifest File:** Defines the app's name, icons, and other settings for a better user experience.
+- **Glitch:** Hosting the project on [Glitch](https://glitch.com/).
+- **Abstract API:** Integration with the [Abstract API](https://app.abstractapi.com/users/signup) for currency exchange rates.
+
+## Getting Started
+
+### Prerequisites
+
+Before you begin, make sure you have the following:
+
+- **Abstract Currency API Key:** Obtain your API key from [AbstractApi.com](https://app.abstractapi.com/users/signup) by signing up for an account. Once you have your API key, you'll be able to fetch currency conversion rates for your app.
+
+- Ensure you have a modern web browser to run the app.
+
+- Active internet connection to fetch currency conversion rates.
+
+### Installation
+
+Follow these steps to run Currencify locally:
+
+1. **Clone the Repository:**
+   ```bash
+   git clone https://github.com/LakhindarPal/Currencify.git
+   ```
+
+2. **Navigate to the Project Directory:**
+   ```bash
+   cd Currencify
+   ```
+3. Fill the `ApiKey` in the `main.js` file.
+
+4. **Open the App:** Open the `index.html` file in your preferred web browser.
+
+Now, you should have Currencify up and running locally.
+
+## How to Use
+
+For detailed instructions on how to use the currency converter, refer to the "How to use" section in the app or visit [Currencify](https://currencify.glitch.me).
+
+## Contributing
+
+If you have suggestions or find any issues, feel free to open an [issue](https://github.com/LakhindarPal/Currencify/issues) or submit a [pull request](https://github.com/LakhindarPal/Currencify/pulls).
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ main.js
@@ -0,0 +1,183 @@
+const API_KEY = "replace_with_your_api_key";
+
+const createElement = (tag) => document.createElement(tag);
+const getElement = (id) => document.getElementById(id);
+
+const elements = {
+  baseCurrencyInput: getElement("baseCurrencyInput"),
+  baseCurrencyOptions: getElement("baseCurrencyOptions"),
+  targetCurrencyInput: getElement("targetCurrencyInput"),
+  targetCurrencyOptions: getElement("targetCurrencyOptions"),
+  amountInput: getElement("amountInput"),
+  convertBtn: getElement("convertBtn"),
+  resultDiv: getElement("resultDiv"),
+  swapBtn: getElement("swapBtn"),
+  resetBtn: getElement("resetBtn"),
+  modalCloseBtn: getElement("modalCloseBtn"),
+  dialog: getElement("instructions"),
+};
+
+const currencies = [
+  { code: "ARS", name: "Argentine Peso" },
+  { code: "AUD", name: "Australian Dollar" },
+  { code: "BCH", name: "Bitcoin Cash" },
+  { code: "BGN", name: "Bulgarian Lev" },
+  { code: "BNB", name: "Binance Coin" },
+  { code: "BRL", name: "Brazilian Real" },
+  { code: "BTC", name: "Bitcoin" },
+  { code: "CAD", name: "Canadian Dollar" },
+  { code: "CHF", name: "Swiss Franc" },
+  { code: "CNY", name: "Chinese Yuan" },
+  { code: "CZK", name: "Czech Republic Koruna" },
+  { code: "DKK", name: "Danish Krone" },
+  { code: "DOGE", name: "Dogecoin" },
+  { code: "DZD", name: "Algerian Dinar" },
+  { code: "ETH", name: "Ethereum" },
+  { code: "EUR", name: "Euro" },
+  { code: "GBP", name: "British Pound Sterling" },
+  { code: "HKD", name: "Hong Kong Dollar" },
+  { code: "HRK", name: "Croatian Kuna" },
+  { code: "HUF", name: "Hungarian Forint" },
+  { code: "IDR", name: "Indonesian Rupiah" },
+  { code: "ILS", name: "Israeli New Sheqel" },
+  { code: "INR", name: "Indian Rupee" },
+  { code: "ISK", name: "Icelandic Króna" },
+  { code: "JPY", name: "Japanese Yen" },
+  { code: "KRW", name: "South Korean Won" },
+  { code: "LTC", name: "Litecoin" },
+  { code: "MAD", name: "Moroccan Dirham" },
+  { code: "MXN", name: "Mexican Peso" },
+  { code: "MYR", name: "Malaysian Ringgit" },
+  { code: "NOK", name: "Norwegian Krone" },
+  { code: "NZD", name: "New Zealand Dollar" },
+  { code: "PHP", name: "Philippine Peso" },
+  { code: "PLN", name: "Polish Zloty" },
+  { code: "RON", name: "Romanian Leu" },
+  { code: "RUB", name: "Russian Ruble" },
+  { code: "SEK", name: "Swedish Krona" },
+  { code: "SGD", name: "Singapore Dollar" },
+  { code: "THB", name: "Thai Baht" },
+  { code: "TRY", name: "Turkish Lira" },
+  { code: "TWD", name: "New Taiwan Dollar" },
+  { code: "USD", name: "US Dollar" },
+  { code: "XRP", name: "Ripple" },
+  { code: "ZAR", name: "South African Rand" }
+  ];
+
+
+let baseCurrency = null;
+let targetCurrency = null;
+
+const createApiUrl = () => `https://exchange-rates.abstractapi.com/v1/convert?api_key=${API_KEY}&base=${baseCurrency.code}&target=${targetCurrency.code}&base_amount=${elements.amountInput.value}`;
+
+const filterOptions = (searchTerm, currencyOptions) => currencyOptions.filter(currency =>
+  currency.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+  currency.code.toLowerCase().includes(searchTerm.toLowerCase())
+);
+
+const updateOptionsList = (inputElement, optionsList, currencyOptions) => {
+  const searchTerm = inputElement.value;
+  const filteredOptions = filterOptions(searchTerm, currencyOptions);
+  optionsList.innerHTML = "";
+
+  filteredOptions.forEach(currency => {
+    const liElement = createElement("li");
+    liElement.textContent = `${currency.name} (${currency.code})`;
+    liElement.classList.add("option");
+    liElement.addEventListener("click", () => selectOption(inputElement, currency));
+    optionsList.appendChild(liElement);
+  });
+
+  optionsList.style.display = filteredOptions.length ? "inline-block" : "none";
+};
+
+const selectOption = (inputElement, currency) => {
+  inputElement.value = `${currency.name} (${currency.code})`;
+
+  if (inputElement.id === "baseCurrencyInput") {
+    baseCurrency = currency;
+  } else if (inputElement.id === "targetCurrencyInput") {
+    targetCurrency = currency;
+  }
+
+  inputElement.nextElementSibling.style.display = "none";
+};
+
+const handleDocumentClick = (event, inputElement, optionsList) => {
+  const isClickInside = inputElement.contains(event.target) || optionsList.contains(event.target);
+  if (!isClickInside) {
+    optionsList.style.display = "none";
+  }
+};
+
+const convertCurrency = () => {
+  const amount = parseFloat(elements.amountInput.value);
+  if (isNaN(amount) || amount <= 0 || !baseCurrency || !targetCurrency) {
+    elements.resultDiv.innerHTML = "<p>Invalid input. Please enter a valid amount and select both base and target currencies.</p>";
+    elements.resultDiv.style.display = "block";
+    elements.resultDiv.style.color = "orangered";
+    return;
+  }
+
+  fetch(createApiUrl())
+    .then(response => response.json())
+    .then(data => {
+      const convertedAmount = data.converted_amount.toFixed(2);
+      const exchangeRate = data.exchange_rate.toFixed(4);
+
+      elements.resultDiv.innerHTML =
+        `<p>Exchange rate:</p>
+        <p>1 ${baseCurrency.code} = ${exchangeRate} ${targetCurrency.code}</p>
+        <hr>
+         <p><strong>Result:</strong></p>
+         <h2>${amount} ${baseCurrency.code} = ${convertedAmount} ${targetCurrency.code}</h2>`;
+
+      elements.resultDiv.style.display = "block";
+      elements.resultDiv.style.color = "inherit";
+    })
+    .catch(error => {
+      elements.resultDiv.innerHTML = "<p>Error fetching conversion rate. Please try again later.</p>";
+      elements.resultDiv.style.display = "block";
+      elements.resultDiv.style.color = "orangered";
+      console.error(error);
+    });
+};
+
+const swapCurrencies = () => {
+  const temp = baseCurrency;
+  baseCurrency = targetCurrency;
+  targetCurrency = temp;
+
+  elements.baseCurrencyInput.value = baseCurrency ? `${baseCurrency.name} (${baseCurrency.code})` : "";
+  elements.targetCurrencyInput.value = targetCurrency ? `${targetCurrency.name} (${targetCurrency.code})` : "";
+
+  elements.resultDiv.innerHTML = "";
+  elements.resultDiv.style.display = "none";
+};
+
+elements.resetBtn.addEventListener("click", () => {
+  baseCurrency = null;
+  targetCurrency = null;
+  elements.amountInput.value = "";
+  elements.baseCurrencyInput.value = "";
+  elements.targetCurrencyInput.value = "";
+  elements.resultDiv.innerHTML = "";
+  elements.resultDiv.style.display = "none";
+});
+
+elements.baseCurrencyInput.addEventListener("input", () => updateOptionsList(elements.baseCurrencyInput, elements.baseCurrencyOptions, currencies));
+elements.targetCurrencyInput.addEventListener("input", () => updateOptionsList(elements.targetCurrencyInput, elements.targetCurrencyOptions, currencies));
+
+elements.baseCurrencyInput.addEventListener("focus", () => updateOptionsList(elements.baseCurrencyInput, elements.baseCurrencyOptions, currencies));
+elements.targetCurrencyInput.addEventListener("focus", () => updateOptionsList(elements.targetCurrencyInput, elements.targetCurrencyOptions, currencies));
+
+document.addEventListener("click", (event) => {
+  handleDocumentClick(event, elements.baseCurrencyInput, elements.baseCurrencyOptions);
+  handleDocumentClick(event, elements.targetCurrencyInput, elements.targetCurrencyOptions);
+});
+
+elements.convertBtn.addEventListener("click", convertCurrency);
+elements.swapBtn.addEventListener("click", swapCurrencies);
+elements.modalCloseBtn.addEventListener("click", () => {
+  elements.dialog.close();
+});
